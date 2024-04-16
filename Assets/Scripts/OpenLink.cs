adc384eaaae22894507fdcee0f899983fbbdaaf8
posleFanti <boxalex2014@gmail.com>
2024-04-16 21:12:23 +0300

    Added highlighting for colliders
    Also improved sideways car movement

--- Assets/Scripts/OpenLink.cs
+++ Assets/Scripts/OpenLink.cs
@@ -1,31 +1,23 @@
-using System;
 using UnityEngine;
 
 public class OpenLink : MonoBehaviour
 {
     [SerializeField] private string url;
     [SerializeField] private Animator animator;
-    private ColorUIChange colorUIChange;
-
-    private void Start()
-    {
-        colorUIChange = GetComponent<ColorUIChange>();
-    }
 
     private void OnTriggerEnter(Collider other)
     {
-        //animator.Play("Base Layer.Highlighting");
+        animator.SetBool("isActivated", true);
     }
 
     private void OnTriggerStay(Collider other)
     {
-        //colorUIChange.ChangeColor();
         if (Input.GetKey(KeyCode.E))
             Application.OpenURL(url);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        //colorUIChange.ResetAlpha();
+        animator.SetBool("isActivated", false);
     }
 }
